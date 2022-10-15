8bf4266c96ea1ae4772e858076db23b59fd2b752
Awbugl <littlenine12@163.com>
2022-10-16 01:04:15 +0800

    Update Code

--- Program.cs
+++ Program.cs
@@ -1,14 +1,15 @@
 using System.Net;
 using ArcaeaUnlimitedAPI.Core;
+using ArcaeaUnlimitedAPI.PublicApi;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.HttpOverrides;
 using Newtonsoft.Json;
 
-AppDomain.CurrentDomain.UnhandledException += (_, eventArgs) => Log.ExceptionError((eventArgs.ExceptionObject as Exception)!);
 TaskScheduler.UnobservedTaskException += (_, eventArgs) =>
-                                         {
-                                             Log.ExceptionError(eventArgs.Exception.InnerException!);
-                                             eventArgs.SetObserved();
-                                         };
+{
+    Log.ExceptionError(eventArgs.Exception.InnerException!);
+    eventArgs.SetObserved();
+};
 
 ServicePointManager.DefaultConnectionLimit = 64;
 ServicePointManager.ReusePort = true;
@@ -16,14 +17,25 @@ ServicePointManager.ReusePort = true;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers().AddNewtonsoftJson(options => options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore)
        .ConfigureApiBehaviorOptions(options =>
-                                    {
-                                        options.SuppressModelStateInvalidFilter = true;
-                                        options.SuppressMapClientErrors = true;
-                                    });
+        {
+            options.SuppressModelStateInvalidFilter = true;
+            options.SuppressMapClientErrors = true;
+        });
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddCors(options => options.AddDefaultPolicy(i => i.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
 var app = builder.Build();
 app.UseCors();
+
+app.UseExceptionHandler(build => build.Run(async context =>
+{
+    var ex = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+    if (ex != null) Log.ExceptionError(ex);
+    context.Response.StatusCode = 500;
+    context.Response.ContentType = "application/json";
+    await context.Response.WriteAsync(JsonConvert.SerializeObject(Response.Error.InternalErrorOccurred));
+}));
+
 app.UseForwardedHeaders(new() { ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto });
 app.MapControllers();
 app.Run();
