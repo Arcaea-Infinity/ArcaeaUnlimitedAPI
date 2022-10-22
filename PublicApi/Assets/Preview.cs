711d9d653f441d8328d6fc46745b2c09535cdf67
Awbugl <littlenine12@163.com>
2022-10-22 23:24:30 +0800

    Update .gitignore

---
+++ PublicApi/Assets/Preview.cs
@@ -0,0 +1,24 @@
+﻿using ArcaeaUnlimitedAPI.Beans;
+using ArcaeaUnlimitedAPI.Core;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using static ArcaeaUnlimitedAPI.PublicApi.Response;
+
+namespace ArcaeaUnlimitedAPI.PublicApi;
+
+public partial class PublicApi
+{
+    [EnableCors]
+    [SongInfoConverter(Order = 0)]
+    [DifficultyConverter(Order = 1)]
+    [ChartConverter(Order = 2)]
+    [HttpGet("/botarcapi/assets/preview")]
+    public object GetPreviewAssets([BindNever] ArcaeaCharts chart)
+    {
+        FileInfo fileinfo = new($"{GlobalConfig.Config.DataPath}/source/preview/{chart.SongID}/{chart.RatingClass}.jpeg");
+        if (!fileinfo.Exists) return NotFound(Error.FileUnavailable);
+
+        return PhysicalFile(fileinfo.FullName, "image/jpeg");
+    }
+}
