dfab0b3d5bc991b86d2e46fe3aa3fc9157d1aff3
Awbugl <littlenine12@163.com>
2022-10-23 19:03:12 +0800

    Fix Version Issue in `data/theory` API

--- PublicApi/Data/TheoryBests.cs
+++ PublicApi/Data/TheoryBests.cs
@@ -43,7 +43,7 @@ public sealed partial class PublicApi
         {
             if (results.Count >= count) break;
 
-            if (double.IsNaN(verNum) || double.Parse(chart.Version) < verNum)
+            if (double.IsNaN(verNum) || double.Parse(chart.Version) <= verNum)
             {
                 var result = new Records
                              {
