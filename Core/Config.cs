e03ce0b4cec9beb3d46b6cd86970042a6d4a6596
Awbugl <littlenine12@163.com>
2022-11-09 22:15:54 +0800

    Update Config.cs

--- Core/Config.cs
+++ Core/Config.cs
@@ -60,34 +60,34 @@ internal static class GlobalConfig
 public class ConfigItem
 {
     [JsonProperty("api_entry")]
-    public string ApiEntry { get; set; }
+    public string ApiEntry { get; set; } = string.Empty;
 
     [JsonProperty("app_version")]
-    public string Appversion { get; set; }
+    public string Appversion { get; set; } = string.Empty;
 
     [JsonProperty("host")]
-    public string Host { get; set; }
+    public string Host { get; set; } = string.Empty;
 
     [JsonProperty("cert_name")]
-    public string CertFileName { get; set; }
+    public string CertFileName { get; set; } = string.Empty;
 
     [JsonProperty("cert_password")]
-    public string CertPassword { get; set; }
+    public string CertPassword { get; set; } = string.Empty;
 
     [JsonProperty("data_path")]
-    public string DataPath { get; set; }
+    public string DataPath { get; set; } = Path.Combine(AppContext.BaseDirectory, "data");
 
     [JsonProperty("open_register")]
-    public bool? OpenRegister { get; set; }
+    public bool? OpenRegister { get; set; } = false;
 
     [JsonProperty("quota")]
     public int Quota { get; set; } = 10;
 
     [JsonConverter(typeof(BytesConverter))] [JsonProperty("api_salt")]
-    public byte[] ApiSalt { get; set; }
+    public byte[] ApiSalt { get; set; } = Array.Empty<byte>();
 
     [JsonProperty("node")]
-    public Node Node { get; set; }
+    public Node Node { get; set; } = new();
 
     internal void WriteConfig() => File.WriteAllText("apiconfig.json", JsonConvert.SerializeObject(this, Formatting.Indented));
 }
@@ -95,10 +95,10 @@ public class ConfigItem
 public class Node
 {
     [JsonProperty("url")]
-    public string Url { get; set; }
+    public string Url { get; set; } = string.Empty;
 
     [JsonProperty("port")]
-    public int? Port { get; set; }
+    public int? Port { get; set; } = 443;
 
     public override string ToString() => $"https://{Url}:{Port ?? 443}";
 }
