53b2b957c451be59998577855941d6f1faad9a63
Awbugl <littlenine12@163.com>
2022-11-09 22:15:54 +0800

    Update Config.cs

--- Core/Config.cs
+++ Core/Config.cs
@@ -60,40 +60,40 @@ internal static class GlobalConfig
 public class ConfigItem
 {
     [JsonProperty("api_entry")]
-    public string ApiEntry { get; set; }
+    public string ApiEntry { get; set; } = string.Empty;
 
     [JsonProperty("app_version")]
-    public string Appversion { get; set; }
+    public string Appversion { get; set; } = string.Empty;
 
     [JsonProperty("host")]
-    public string Host { get; set; }
+    public string Host { get; set; } = string.Empty;
 
     [JsonProperty("cert_name")]
-    public string CertFileName { get; set; }
+    public string CertFileName { get; set; } = string.Empty;
 
     [JsonProperty("cert_password")]
-    public string CertPassword { get; set; }
+    public string CertPassword { get; set; } = string.Empty;
 
     [JsonProperty("data_path")]
-    public string DataPath { get; set; }
+    public string DataPath { get; set; } = Path.Combine(AppContext.BaseDirectory, "data");
 
     [JsonProperty("open_register")]
-    public bool? OpenRegister { get; set; }
+    public bool? OpenRegister { get; set; } = false;
 
     [JsonProperty("quota")]
     public int Quota { get; set; } = 10;
 
     [JsonProperty("challenge_type")]
-    public string ChallengeType { get; set; }
+    public string ChallengeType { get; set; } = string.Empty;
 
     [JsonProperty("challenge_api")]
-    public string ChallengeUrl { get; set; }
+    public string ChallengeUrl { get; set; } = string.Empty;
 
     [JsonProperty("challenge_token")]
-    public string ChallengeToken { get; set; }
-
+    public string ChallengeToken { get; set; } = string.Empty;
+ 
     [JsonProperty("nodes")]
-    public List<Node> Nodes { get; set; }
+    public List<Node> Nodes { get; set; } = new() { new() };
     
     internal void WriteConfig() => File.WriteAllText("apiconfig.json", JsonConvert.SerializeObject(this, Formatting.Indented));
 }
@@ -101,10 +101,10 @@ public class ConfigItem
 public class Node
 {
     [JsonProperty("url")]
-    public string Url { get; set; }
+    public string Url { get; set; } = string.Empty;
 
     [JsonProperty("port")]
-    public int? Port { get; set; }
+    public int? Port { get; set; } = 443;
 
     [JsonProperty("active")]
     internal bool Active { get; set; } = true;
