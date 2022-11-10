77bcdf24f39e157d57e4821b52ce79330aaacc94
Awbugl <littlenine12@163.com>
2022-11-10 11:34:13 +0800

    Create Cert.cs

---
+++ PublicApi/Data/Cert.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static ArcaeaUnlimitedAPI.Core.GlobalConfig;
+using static ArcaeaUnlimitedAPI.PublicApi.Response;
+
+namespace ArcaeaUnlimitedAPI.PublicApi;
+
+public sealed partial class PublicApi
+{
+    [AuthorizationCheck]
+    [HttpGet("/botarcapi/data/cert")]
+    public async Task<object> GetCert()
+    {
+        var certstr = Convert.ToBase64String(await System.IO.File.ReadAllBytesAsync(Path.Combine(Config.DataPath, Config.CertFileName)));
+        return Success(new { name = Config.CertFileName, cert = certstr });
+    }
+}
